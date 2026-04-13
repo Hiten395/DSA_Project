e5b759154a9ec89f64f2f0cdc4472e82b7b21e16
Aditya Akash <adityaakash247@gmail.com>
2026-04-13 22:55:09 +0530

    Refactor DataBase.cpp to manage data structures

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <queue>
+#include <stack>
 #include "httplib.h"
 #include "nlohmann/json.hpp"
 
 using json = nlohmann::json;
 
-// all user functions
+// Global data structures
+std::stack<int> st;
+std::queue<int> qu;
+std::vector<int> ll;
 
+int currentDS = 0; // 1 = Stack, 2 = Queue, 3 = Linked List
+int maxSize = 0;
+
+// Helper functions
+bool isFull() {
+    if (currentDS == 1) return st.size() >= maxSize;
+    if (currentDS == 2) return qu.size() >= maxSize;
+    if (currentDS == 3) return ll.size() >= maxSize;
+    return false;
+}
+
+bool isEmpty() {
+    if (currentDS == 1) return st.empty();
+    if (currentDS == 2) return qu.empty();
+    if (currentDS == 3) return ll.empty();
+    return true;
+}
 
 int main()
 {
-    // DO NOT CHANGE
     httplib::Server server;
 
-    // Add CORS headers to ALL responses DO NOT CHANGE
     server.set_default_headers({
         {"Access-Control-Allow-Origin", "*"},
         {"Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"},
@@ -27,85 +48,109 @@ int main()
         int size = std::stoi(req.path_params.at("size"));
 
         std::cout << "Operation: " << opr << " Value: " << val << " Size: " << size << std::endl;
-        // values from the client are stored in the above variables and printed to console
 
         json response;
+
         if (opr == "New")
         {
-            // user has to choose the type of data structure of size 'size'
+            currentDS = val;
+            maxSize = size;
+
+            // Clear all structures
+            while (!st.empty()) st.pop();
+            while (!qu.empty()) qu.pop();
+            ll.clear();
+
             if (val == 1)
-            {
-                // user has chosen to make a new Stack
-                response = {{"message", "To New Stack"}};
-            }
+                response = {{"message", "New Stack Created"}};
             else if (val == 2)
-            {
-                // user has chosen to make a new Queue
-                response = {{"message", "To New Queue"}};
-            }
+                response = {{"message", "New Queue Created"}};
             else if (val == 3)
-            {
-                // user has chosen to make a new Linked List
-                response = {{"message", "To New Linked List"}};
-            }
+                response = {{"message", "New Linked List Created"}};
             else
-            {
                 response = {{"message", "Invalid Value"}};
-            }
         }
         else if (opr == "Push")
         {
-            // to push to whatever datastructure exists
-            response = {{"message", "To Push"}};
-
-            int a;
-            // store the value in a returned by push of whatever datastructure exists and uncomment next line
-            // response = {{"message", a}};
+            if (isFull()) {
+                response = {{"message", "Structure is Full"}};
+            } else {
+                if (currentDS == 1) {
+                    st.push(val);
+                } else if (currentDS == 2) {
+                    qu.push(val);
+                } else if (currentDS == 3) {
+                    ll.push_back(val);
+                }
+                response = {{"message", "Pushed Successfully"}};
+            }
         }
         else if (opr == "Pop")
         {
-            // to pop from whatever datastructure exists
-            response = {{"message", "To Pop"}};
-
-            int a;
-            // store the value in a returned by pop of whatever datastructure exists and uncomment next line
-            // response = {{"message", a}};
+            if (isEmpty()) {
+                response = {{"message", "Structure is Empty"}};
+            } else {
+                int a;
+                if (currentDS == 1) {
+                    a = st.top();
+                    st.pop();
+                } else if (currentDS == 2) {
+                    a = qu.front();
+                    qu.pop();
+                } else if (currentDS == 3) {
+                    a = ll.back();
+                    ll.pop_back();
+                }
+                response = {{"message", a}};
+            }
         }
         else if (opr == "Peek")
         {
-            // to peek from whatever datastructure exists
-            response = {{"message", "To Peek"}};
-
-            int a;
-            // store the value in a returned by peek of whatever datastructure exists and uncomment next line
-            // response = {{"message", a}};
+            if (isEmpty()) {
+                response = {{"message", "Structure is Empty"}};
+            } else {
+                int a;
+                if (currentDS == 1) {
+                    a = st.top();
+                } else if (currentDS == 2) {
+                    a = qu.front();
+                } else if (currentDS == 3) {
+                    a = ll.back();
+                }
+                response = {{"message", a}};
+            }
         }
         else if (opr == "Display")
         {
-            // to display whatever datastructure exists
-            response = {{"message", "To Display"}};
-
             std::vector<int> data;
-            // store the value of whateber database in data and uncomment next line
-            // response = {{"message", data}};
+
+            if (currentDS == 1) {
+                std::stack<int> temp = st;
+                while (!temp.empty()) {
+                    data.push_back(temp.top());
+                    temp.pop();
+                }
+            } 
+            else if (currentDS == 2) {
+                std::queue<int> temp = qu;
+                while (!temp.empty()) {
+                    data.push_back(temp.front());
+                    temp.pop();
+                }
+            } 
+            else if (currentDS == 3) {
+                data = ll;
+            }
+
+            response = {{"message", data}};
         }
         else if (opr == "Full")
         {
-            // to check if the datastructure is full
-            response = {{"message", "To Full"}};
-            
-            bool isFull;
-            // store the value of full in isFull and uncomment next line
-            //response = {{"message", isFull}}
+            response = {{"message", isFull()}};
         }
         else if (opr == "Empty")
         {
-            // to check if the datastructure is empty
-            response = {{"message", "To Empty"}};
-
-            bool isEmpty;
-            // store the value of empty in isEmpty and uncomment next line
-            //response = {{"message", isEmpty}}
+            response = {{"message", isEmpty()}};
         }
         else
         {
@@ -113,12 +158,8 @@ int main()
         }
 
         res.set_content(response.dump(), "application/json");
-
     });
 
-
-    // Do not change anything below this line
-    
     std::cout << "Server listening on port 8080" << std::endl;
     server.listen("0.0.0.0", 8080);
 
